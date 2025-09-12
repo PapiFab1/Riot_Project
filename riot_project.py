532c5d2984573a0830356894528489dfc56e73e7
PapiFab1 <144495327+PapiFab1@users.noreply.github.com>
2025-09-12 13:03:42 -0400

    second commit

---
+++ riot_project.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import requests as rq
+from dotenv import load_dotenv
+import os
+
+#loading .env
+load_dotenv()
+
+#for now a simple function that gets the player name and tag to make it more user friendly (will need to make this work for website)
+def account_info():
+    game_name = input("Enter your account name: ")
+    tag_name = input("Enter your tag: ")
+    return game_name,tag_name
+    
+
+def playerID_API():
+    api_key = (os.getenv("riot_api_key"))
+    base_player_id_url = "https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/"
+    url = f"{base_player_id_url}{game_name}/{tag}/?api_key={api_key}"
+    print(url)
+
+    response = rq.get(url)
+    player_id = response.json()
+    print(player_id)
+
+game_name, tag = account_info()
+playerID_API()
+
+
+
