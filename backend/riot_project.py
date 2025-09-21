1d844afd4cc44d5e516688c660b44d8399d0e437
PapiFab1 <144495327+PapiFab1@users.noreply.github.com>
2025-09-20 20:01:15 -0400

    added summary and changed data to be stored in dictionary

--- backend/riot_project.py
+++ backend/riot_project.py
@@ -39,7 +39,7 @@ def match_ID_API(playerID):
 
    
 #only does one match at a time for now. Will need to update to iterate through 5 matches
-def player_stats_API(match_ID, player_ID):
+def player_stats_API(match_ID, player_ID,data_dict):
     api_key = os.getenv("riot_api_key")
     base_url = "https://americas.api.riotgames.com/lol/match/v5/matches/"
     url = f"{base_url}{match_ID}/?api_key={api_key}"
@@ -49,29 +49,64 @@ def player_stats_API(match_ID, player_ID):
     data = response.json()
     participants = data["metadata"]["participants"]
     player_index = participants.index(player_ID)
-    print(data["info"]["participants"][player_index]["summonerName"])
-    print(" ")
+    #print(data["info"]["participants"][player_index]["summonerName"])
+    
     
     c = data["info"]["participants"][player_index]["championName"]
     k = data["info"]["participants"][player_index]["kills"]
     d = data["info"]["participants"][player_index]["deaths"]
     a = data["info"]["participants"][player_index]["assists"]
-
-    print(f"Champion: {c}")
-    print(f"Kills: {k}")
-    print(f"Deaths: {d}")
-    print(f"Assists: {a}\n")
+    w = data["info"]["participants"][player_index]["win"]
 
 
+    # print(f"Champion: {c}")
+    # print(f"Kills: {k}")
+    # print(f"Deaths: {d}")
+    # print(f"Assists: {a}\n")
+    # dataDict["Champion"].appendd({"Champion":c,"Kills": k, "Deaths": d, "Assists": a})
+
+    #puts data into dictionary
+    data_dict["Champion"].append(c)
+    data_dict["Kills"].append(k)
+    data_dict["Deaths"].append(d)
+    data_dict["Assists"].append(a)
+    data_dict["Win"].append(w)
+
+
+
+data_dict = {
+    "Champion": [],
+    "Kills": [],
+    "Deaths": [],
+    "Assists": [],
+    "Win":[]
+}
 game_name, tag = account_info()
 playerID = playerID_API()
 match_ID = match_ID_API(playerID)
 
+
 for i in match_ID:
-    player_stats_API(i,playerID)
+    player_stats_API(i,playerID,data_dict)
 
+df = pd.DataFrame(data_dict)
+df.index.name = "Game Number"
+#calculates and adds KDA to dataframe
+df["KDA"] = (df["Kills"] + df["Assists"]) / df["Deaths"].replace(0, 1)
+best_kda = df.loc[df["KDA"].idxmax()]
+worst_kda = df.loc[df["KDA"].idxmin()]
+most_used_champ = df["Champion"].mode()[0]
 
-    
+summary = {
+    "Best KDA": best_kda["KDA"],
+    "Best KDA Champion": best_kda["Champion"],
+    "Worst KDA": worst_kda["KDA"],
+    "Worst KDA Champion": worst_kda["Champion"],
+    "Most Used Champion": most_used_champ
+}
+
+print(df)
+print(summary)
     
 
 
---
+++ backend/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+ dataDict["Champion"].append(c)
